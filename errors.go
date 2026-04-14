2b03d7c554f40f68c801aaf8e2ff6be3ad8e7369
AndrewSr2pos <andrew.sherman@r2pos.com>
2026-04-14 15:37:14 -0400

    created new error type

--- errors.go
+++ errors.go
@@ -4,7 +4,7 @@ import "errors"
 
 var (
 	ErrInvalidBalenaDeviceUUID = errors.New("invalid balena device uuid")
-
+	ErrInvalidMerchantUUID     = errors.New("invalid merchant uuid")
 	ErrResourceNotFound  = errors.New("resource not found")
 	ErrExpectedOneResult = errors.New("expected one result")
 	ErrEnvVarNotFound    = errors.New("env var not found")
